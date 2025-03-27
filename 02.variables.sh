a6739fc0d5e627577d815aa79667929836439d27
Kalyan Kumar Voleti <kalyankumar.voleti@gmail.com>
2025-03-27 18:13:20 +0530

    conservation is added

--- 02.variables.sh
+++ 02.variables.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-echo "this is a variable example"
+echo "Robert::How are you Caven?"
+echo "Caven:: Iam fine Robert what about you?"
+echo "Robert:: ok iam also fine Caven"
+echo "Caven:: i want to meet you Robert"
+echo "Robert:: sure Caveen"
+echo "Caven:: thanks, Rober"
 
