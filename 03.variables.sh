4c94c3d62741016ec388d54261d2d4a2ad21c86f
Kalyan Kumar Voleti <kalyankumar.voleti@gmail.com>
2025-03-27 18:35:27 +0530

    new

--- 03.variables.sh
+++ 03.variables.sh
@@ -4,7 +4,7 @@ PERSON1=Suresh
 PERSON2=Ramesh
 
 echo "$PERSON1:: How are you $PERSON2"
-echo "$PERSON2:: Hay Hii $PERSON1"
-echo "$PERSON1:: Hii dude"
+echo "${PERSON2}:: Hay Hii $PERSON1"
+echo "${PERSON1}:: Hii dude"
 
 
