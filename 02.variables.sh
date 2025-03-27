465c59fcbf81f4ab7f54d84cbfb939a6d5f07356
Kalyan Kumar Voleti <kalyankumar.voleti@gmail.com>
2025-03-27 17:46:31 +0530

    New conversation is added

--- 02.variables.sh
+++ 02.variables.sh
@@ -5,4 +5,5 @@ echo "Caven:: Iam fine Robert what about you?"
 echo "Robert:: ok iam also fine Caven"
 echo "Caven:: i want to meet you Robert"
 echo "Robert:: sure Caveen"
+echo "Caven:: thanks, Rober"
 
