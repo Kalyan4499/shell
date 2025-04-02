5b1a1ee6ec63085f5be049e5b7075177bc2b06a0
Kalyan Kumar Voleti <kalyankumar.voleti@gmail.com>
2025-04-02 13:16:17 +0530

    special vars

--- 08.special vars.sh
+++ 08.special vars.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-
+sleep 20 &
 echo "all arguments passed:: $@"
 echo "number of arguments passed:: $#"
 echo "script name:: $0"
 echo "process id of the current script:: $$"
 echo "process id of the last command u execute in the background:: $!"
 echo "which user is running the script::$USER"
+dnf list installed git
 echo "which is the present working directory:: $PWD"
 echo "home directory of the current user:: $HOME"
-dnf list installed git
+
 echo "status code of the previous command:: $?"
